1f8f36ab487c3c0815b404a7ab9d57441b00a5b2
nicolasferleite <nicolasferreiraleite@gmail.com>
2023-05-06 18:35:34 -0300

    add

---
+++ basico/sorteio.py
@@ -0,0 +1,7 @@
+import random
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+sorteio = random.choice(n1,n2,n3,n4)
+print(sorteio)
